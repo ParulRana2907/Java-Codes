ecae62a27f8fbbf28206acfa945e31f69f729065
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:39:11 +0530

    Q5.java
    .java

--- Practical 2/Q5.java
+++ Practical 2/Q5.java
@@ -1,1 +1,22 @@
+//Experiment 2 : to print rhombus
 
+import java.util.Scanner;
+class Rhombus {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars with spaces
+            for (int k = 1; k <= rows; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println(); 
+        }
+    }
+}
