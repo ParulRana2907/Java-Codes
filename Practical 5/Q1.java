e43b645e7a63a3e8a11e9558a26a3a13252c0fa1
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:54:43 +0530

    Q1.java
    .java

--- Practical 5/Q1.java
+++ Practical 5/Q1.java
@@ -1,1 +1,33 @@
-
+//Experiment 5:-Create a class named 'Shape' with a method
+//Q1
+import java.util.Scanner;
+class Shape {
+    void display() {
+        System.out.println("This is shape");
+    }
+}
+class Rectangle extends Shape {
+    void displayRectangle() {
+        System.out.println("This is rectangular shape");
+    }
+}
+class Circle extends Shape {
+    void displayCircle() {
+        System.out.println("This is circular shape");
+    }
+}
+class Square extends Rectangle {
+    void displaySquare() {
+        System.out.println("Square is a rectangle");
+        System.out.println("Parul Rana 24CSU292");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        sq.display();            
+        sq.displayRectangle();     
+       
+        sq.displaySquare();      
+    }
+}
