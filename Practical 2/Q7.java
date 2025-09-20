f0be962d5d96522f569c88050eac33ef1d26a7a9
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:41:10 +0530

    Q7.java
    .java

--- Practical 2/Q7.java
+++ Practical 2/Q7.java
@@ -1,1 +1,23 @@
 
+// Experiment 2: Fibonacci Series
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int terms = input.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
+
+        for (int i = 2; i < terms; i++) {
+            int nextTerm = firstTerm + secondTerm;
+            System.out.print(", " + nextTerm);
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
