37bf6d7e4b5769f19bc939f7da157a465ce9cff8
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:36:02 +0530

    Q2.java
    .java

--- Practical 2/Q2.java
+++ Practical 2/Q2.java
@@ -1,1 +1,39 @@
+//Experiment 2: to check vowel or consonant
 
+import java.util.Scanner;
+
+class VowelConsonant {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter any alphabet: ");
+        char ch = input.next().charAt(0);
+
+        switch(ch) {
+            case 'a':
+            case 'A':
+                System.out.println("it's a vowel");
+                break;
+            case 'e':
+            case 'E':
+                System.out.println("it's a vowel");
+                break;
+            case 'i':
+            case 'I':
+                System.out.println("it's a vowel");
+                break;
+            case 'o':
+            case 'O':
+                System.out.println("it's a vowel");
+                break;
+            case 'u':
+            case 'U':
+                System.out.println("it's a vowel");
+                break;
+            default:
+                System.out.println("it's a consonant.");
+                break;
+        }
+    }
+}
