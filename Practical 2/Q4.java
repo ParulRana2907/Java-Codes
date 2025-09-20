a282225ab7e976c39150d4a9f5bc3361ba863215
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:38:18 +0530

    Q4.java
    .java

--- Practical 2/Q4.java
+++ Practical 2/Q4.java
@@ -1,1 +1,22 @@
+//experiment - 2
 
+import java.util.Scanner;
+class Pattern {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars with spaces
+            for (int k = 1; k <= 2*i- 1; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println(); 
+        }
+    }
+}
