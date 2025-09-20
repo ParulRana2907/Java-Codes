2a21f4df4b5685f6b2d1034d499c7dfcdd5064d5
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:25:22 +0530

    Q1.java
    .java

--- Practical 1/Q1.java
+++ Practical 1/Q1.java
@@ -1,1 +1,19 @@
+//Experiment 1 : Sum of two numbers 
+
+import java.util.Scanner;
+class Sum
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Parul Rana 24csu292");
+	int a,b, sum;
+	System.out.println("Enter two numbers: ");
+	//System.out.println("Enter your choice: ");
+	Scanner scan = new Scanner(System.in);
+	a = scan.nextInt();
+	b = scan.nextInt();
+	sum = a+b;
+	System.out.println("Sum of a and b is" + sum+ "Program Ended");
+	}
+}
 
