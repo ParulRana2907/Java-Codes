363cfc8435098f75c3a89b55b7c408fe65063862
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:47:02 +0530

    Q5.java
    .java

--- Practical 3/Q5.java
+++ Practical 3/Q5.java
@@ -1,1 +1,35 @@
+// Experiment 3: Reverse an array
 
+import java.util.Scanner;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+    System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Original array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
