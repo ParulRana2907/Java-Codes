266861d9a9adfe16d264fd2a3672a54b19f671fa
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:37:07 +0530

    Q3.java
    .java

--- Practical 2/Q3.java
+++ Practical 2/Q3.java
@@ -1,1 +1,47 @@
+// Experiment 2: Pattern Printing : Pyramid,left triangle, right triangle
 
+import java.util.Scanner;
+
+public class PatternPrinting {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+
+        // Pyramid
+        System.out.println("Pyramid Pattern:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        // Left Triangle
+        System.out.println("\nLeft Triangle Pattern:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        // Right Triangle
+        System.out.println("\nRight Triangle Pattern:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
