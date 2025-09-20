d70d5f50096b57be29c16e7b776af697cffedc61
Parul Rana <parulrana2407@gmail.com>
2025-09-20 23:04:53 +0530

    part4.java
    .java

--- Practical 5/Q3.java/part4.java
+++ Practical 5/Q3.java/part4.java
@@ -1,1 +1,27 @@
+//q3....part 4
+// VehicleApp.java
+import java.util.Scanner;
 
+public class VehicleApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Parul Rana 24csu292");
+        System.out.print("Enter the number of tires: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        Vehicle v;
+        switch (n) {
+            case 2:
+                v = new Bike();
+                break;
+            case 4:
+                v = new Car();
+                break;
+            default:
+                v = new Vehicle();
+                break;
+        }
+        v.display();
+    }
+}
