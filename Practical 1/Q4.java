223209fbfc07556932cb54033452475370e74097
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:31:48 +0530

    Q4.java
    .java

--- Practical 1/Q4.java
+++ Practical 1/Q4.java
@@ -1,1 +1,15 @@
+// Experiment 1: Fahrenheit to Celsius
 
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println(fahrenheit + " F is equal to " + celsius + " C.");
+    }
+}
