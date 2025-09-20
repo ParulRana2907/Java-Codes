61672770196668c0925411365d855646cc5fb91d
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:28:37 +0530

    Q3.java
    .java

--- Practical 1/Q3.java
+++ Practical 1/Q3.java
@@ -1,1 +1,18 @@
+//Experiment 1 : Minutes into years, months and days
 
+import java.util.Scanner;
+
+public class TimeConverter {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of minutes: ");
+        long minutes = input.nextLong();
+
+        long years = minutes / (60 * 24 * 365);
+        long remainingMinutes = minutes % (60 * 24 * 365);
+        long days = remainingMinutes / (60 * 24);
+
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
+    }
+}
