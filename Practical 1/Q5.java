646ca8e273e09dbbb10edabb76ae0e817fe5e265
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:32:18 +0530

    Q5.java
    .java

--- Practical 1/Q5.java
+++ Practical 1/Q5.java
@@ -1,1 +1,23 @@
+// Experiment 1: Sum of a 3-digit number
 
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println("Parul Rana 24csu292");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a 3-digit number: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        int temp = number;
+
+        sum += temp % 10;
+        temp /= 10;
+        sum += temp % 10;
+        temp /= 10;
+        sum += temp % 10;
+
+        System.out.println("The sum of the digits is: " + sum);
+    }
+}
