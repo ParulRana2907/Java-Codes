47795a806857c65425fba9301450ad64032d2dbc
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:59:03 +0530

    part2.java
    .java

--- Practical 5/Q2.java/part2.java
+++ Practical 5/Q2.java/part2.java
@@ -1,1 +1,27 @@
+//que2....part 2
+// BoxWeight.java
+class BoxWeight extends Box {
+    float weight;
 
+    BoxWeight() {
+        super();
+        this.weight = 0;
+    }
+
+    BoxWeight(float w, float h, float d, float m) {
+        super(w, h, d);
+        this.weight = m;
+    }
+
+    BoxWeight(BoxWeight ob) {
+        super(ob);
+        this.weight = ob.weight;
+    }
+
+    public float getWeight() { return weight; }
+    public void setWeight(float m) { this.weight = m; }
+
+    void displayWeight() {
+        System.out.println("Weight is " + weight);
+    }
+}
