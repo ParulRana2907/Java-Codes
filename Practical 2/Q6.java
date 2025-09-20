80db199d3465f3bdfc7c33b4c14884de651ba3a5
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:40:02 +0530

    Q6.java
    .java

--- Practical 2/Q6.java
+++ Practical 2/Q6.java
@@ -1,1 +1,32 @@
+//Experiment 2 : Check a number is prime or not
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static boolean isPrime(int n) {
+        System.out.println("Parul Rana 24csu292");
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
