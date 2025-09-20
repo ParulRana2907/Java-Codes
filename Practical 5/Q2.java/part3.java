6d7f5e8915b0d52577cd65c1c53d36da7992c48b
Parul Rana <parulrana2407@gmail.com>
2025-09-20 22:59:58 +0530

    part3.java
    .java

--- Practical 5/Q2.java/part3.java
+++ Practical 5/Q2.java/part3.java
@@ -1,1 +1,26 @@
+//que2....part 3
+// BoxWeightDemo.java
+public class BoxWeightDemo {
+    public static void main(String args[]) {
+        BoxWeight box1 = new BoxWeight(10, 20, 15, 34.3f);
+        BoxWeight box2 = new BoxWeight();
+        BoxWeight box3 = new BoxWeight(box1);
+        System.out.println("Parul Rana")
 
+        System.out.println("Box 1:");
+        box1.volume();
+        box1.displayWeight();
+        
+        System.out.println("\nBox 2:");
+        box2.setWeight(20);
+        box2.displayWeight();
+        box2.setWidth(5);
+        box2.setHeight(5);
+        box2.setDepth(5);
+        box2.volume();
+
+        System.out.println("\nBox 3 (Clone of Box 1):");
+        box3.volume();
+        box3.displayWeight();
+    }
+}
